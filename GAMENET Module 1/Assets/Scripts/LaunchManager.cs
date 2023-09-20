16c3244c4fc871ba90a927dc6bdafd4ca4443efd
dnellecruz <danielle.cruz@benilde.edu.ph>
2023-09-20 23:28:29 +0800

    Module 1

---
+++ GAMENET Module 1/Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    public GameObject EnterGamePanel;
+    public GameObject ConnectionStatusPanel;
+    public GameObject LobbyPanel;
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        EnterGamePanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
+        LobbyPanel.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.NickName + " connected to photon servers.");
+        ConnectionStatusPanel.SetActive(false);
+        LobbyPanel.SetActive(true);
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("connected to internet");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.LogWarning(message);
+        CreateAndJoinRoom();
+    }
+
+    public void ConnectToPhotonServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+
+            EnterGamePanel.SetActive(false);
+            ConnectionStatusPanel.SetActive(true);
+        }
+    }
+
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    private void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room " + Random.Range(0, 10000);
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 20;
+
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.NickName + " has entered " + PhotonNetwork.CurrentRoom.Name);
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " has entered room " + PhotonNetwork.CurrentRoom.Name
+                  + ". Room now has " + PhotonNetwork.CurrentRoom.PlayerCount + " players.");
+    }
+}
---
+++ GAMENET Module 1/Assets/Scripts/TakingDamage.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class TakingDamage : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    Image healthbar;
+
+    private float startHealth = 100;
+    private float health;
+
+    void Start()
+    {
+        health = startHealth;
+        healthbar.fillAmount = health / startHealth;
+    }
+
+    [PunRPC]
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log(health);
+
+        healthbar.fillAmount = health / startHealth;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (photonView.IsMine)
+        {
+            GameManager.instance.LeaveRoom();
+        }
+    }
+}
